3860dcdcd5dd0feb02dc9dfdf04a7907752c1c57
NIRAJ CHOWDHARY <chowdharyniraj97@gmail.com>
2020-03-30 19:32:39 -0400

    reading tables from web

--- ReadTable.py
+++ ReadTable.py
@@ -2,6 +2,11 @@ import pandas as pd
 d = pd.read_html("https://hellnar.github.io/openings/Openings.html")
 
 print(type(d))
-df = d[0]
-print(len(d))
-print(df)
+fname = "openings.txt"
+with open(fname, "w", encoding="utf-8") as f:
+    f.write(str(d))
+
+
+# for table in d:
+#     f.write(str(table))
+#     f.write("\n")
